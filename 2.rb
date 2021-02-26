6a84a79eac76ca39a81cea7b9359d891ec893e07
armidsinner <1986damir55@gmail.com>
2021-02-27 02:20:31 +0300

    Update 2.rb

--- 2.rb
+++ 2.rb
@@ -16,3 +16,27 @@
 #
 ## Решение:
 
+require "digest"
+
+last = gets.chomp
+b = Digest::MD5.hexdigest(last)
+c = 0
+g = 0
+checker = 0
+while c != 1
+  g += 1
+  new =last+g.to_s
+  b = Digest::MD5.hexdigest(new)
+  d = b.split(//)
+  for i in 0..4
+    if d[i] == '0'
+      checker += 1
+    end
+  end
+  if checker == 5
+    c = 1
+  else
+    checker = 0
+  end
+end
+puts (g)
