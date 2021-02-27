532df56d9178a93c289bef0684b7d988a9399be7
armidsinner <1986damir55@gmail.com>
2021-02-27 21:26:05 +0300

    Update 4.rb

--- 4.rb
+++ 4.rb
@@ -15,5 +15,45 @@
 #
 #
 ## Решение:
-
-
+lines = IO.readlines("data/4.txt")
+smallest = 0
+needed = 0
+for i in (0..(lines.length-1))
+  parameters = lines[i].split(/x/).map(&:to_i)
+  length = parameters.min
+  width = parameters.max
+  lw=[length, width ]
+  if parameters.detect{ |e| parameters.count(e) > 1 } == nil
+    parameters=parameters-lw
+    height = parameters[0]
+    height_width = height * width
+    height_length = height * length
+    length_width = length * width
+    if height_width <= height_length and height_width <= length_width
+      smallest = height_width
+    end
+    if height_length <= height_width and height_length <= length_width
+      smallest = height_length
+    end
+    if length_width <= height_length and length_width <= height_width
+      smallest = length_width
+    end
+    needed = needed + 2*width*length + 2*length*height + 2*height*width + smallest
+  else
+    height =  parameters.detect{ |e| parameters.count(e) > 1 }
+    height_width = height * width
+    height_length = height * length
+    length_width = length * width
+    if height_width <= height_length and height_width <= length_width
+      smallest = height_width
+    end
+    if height_length <= height_width and height_length <= length_width
+      smallest = height_length
+    end
+    if length_width <= height_length and length_width <= height_width
+      smallest = length_width
+    end
+    needed = needed + 2*width*length + 2*length*height + 2*height*width + smallest
+  end
+end
+puts needed
